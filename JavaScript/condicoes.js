ea27cdab7b2f1c19c0a9a4f26dd786cb80657f8e
Vinicius Boldrin <126197366+ViniciusBoldrin@users.noreply.github.com>
2024-04-24 15:55:48 -0400

    Create condicoes.js

---
+++ JavaScript/condicoes.js
@@ -0,0 +1,11 @@
+console.log("Trabalhando com condicionais");
+
+const listaDeDestinos = new Array (
+    "São Paulo",
+    "Rio de Janeiro",
+    "Salvador",
+    "Bahia",
+    "Cuiabá"
+);
+
+console.log(`Destinos possíveis: ${listaDeDestinos}`);
